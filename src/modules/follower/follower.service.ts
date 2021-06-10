bdf1963ae00df8d1ed847a6232721cebef1ae869
Yegor Tsukanov <69nonesons@gmail.com>
2021-06-10 16:38:44 +0300

    Add create method to service

--- src/modules/follower/follower.service.ts
+++ src/modules/follower/follower.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Followers } from '../../db/entities/Followers/followers';
+import { CreateFollowerDto } from './dto/create-follower-dto';
 
 @Injectable()
-export class FollowerService {}
+export class FollowerService {
+  constructor(
+    @InjectRepository(Followers)
+    private followersRepository: Repository<Followers>,
+  ) {}
+
+  async create(createFollower: CreateFollowerDto) {
+    return this.followersRepository
+      .save(new Followers(createFollower))
+      .catch((e) => e.message);
+  }
+}

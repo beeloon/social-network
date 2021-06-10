23a36f6e5195459e04d8cf508e67f0ffdb1bb0a0
Yegor Tsukanov <69nonesons@gmail.com>
2021-06-10 16:38:21 +0300

    Add imports to module

--- src/modules/follower/follower.module.ts
+++ src/modules/follower/follower.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { FollowerController } from './follower.controller';
 import { FollowerService } from './follower.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Followers } from '../../db/entities/Followers/followers';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Followers])],
   controllers: [FollowerController],
   providers: [FollowerService],
 })

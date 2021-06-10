c5ed1063cfac5b569def79e48431e7ffd4078e25
Yegor Tsukanov <69nonesons@gmail.com>
2021-06-10 16:38:01 +0300

    Add create method to follower controller

--- src/modules/follower/follower.controller.ts
+++ src/modules/follower/follower.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { FollowerService } from './follower.service';
+import { CreateFollowerDto } from './dto/create-follower-dto';
 
 @Controller('followers')
-export class FollowerController {}
+export class FollowerController {
+  constructor(private readonly followerService: FollowerService) {}
+
+  @Post()
+  create(@Body() createFollowerDto: CreateFollowerDto) {
+    return this.followerService.create(createFollowerDto);
+  }
+}

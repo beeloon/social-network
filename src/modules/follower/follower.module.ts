4e199318902bf5fdd94f6520a56013d86e6929f9
Yegor Tsukanov <69nonesons@gmail.com>
2021-06-11 16:47:53 +0300

    Add Followers entity import to module

--- src/modules/follower/follower.module.ts
+++ src/modules/follower/follower.module.ts
@@ -4,9 +4,13 @@ import { FollowerController } from './follower.controller';
 import { FollowerService } from './follower.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Followers } from '../../db/entities/Followers/followers';
+import { User } from '../../db/entities/Users/user';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Followers])],
+  imports: [
+    TypeOrmModule.forFeature([Followers]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [FollowerController],
   providers: [FollowerService],
 })

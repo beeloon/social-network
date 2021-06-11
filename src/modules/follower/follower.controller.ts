527474a13c16bff2f533749e562b9c07ca72ea04
Yegor Tsukanov <69nonesons@gmail.com>
2021-06-11 16:45:59 +0300

    Add controllers for accept, decline, unfollow endpoints

--- src/modules/follower/follower.controller.ts
+++ src/modules/follower/follower.controller.ts
@@ -9,13 +9,37 @@ import {
 } from '@nestjs/common';
 import { FollowerService } from './follower.service';
 import { CreateFollowerDto } from './dto/create-follower-dto';
+import { Followers } from '../../db/entities/Followers/followers';
 
 @Controller('followers')
 export class FollowerController {
   constructor(private readonly followerService: FollowerService) {}
 
-  @Post()
-  create(@Body() createFollowerDto: CreateFollowerDto) {
+  @Post('follow')
+  create(
+    @Body() createFollowerDto: CreateFollowerDto,
+  ): Promise<Followers | string> {
     return this.followerService.create(createFollowerDto);
+  }
+
+  @Patch('accept')
+  accept(
+    @Body() createFollowerDto: CreateFollowerDto,
+  ): Promise<Followers | string> {
+    return this.followerService.update(createFollowerDto, 'Accepted');
+  }
+
+  @Patch('decline')
+  decline(
+    @Body() createFollowerDto: CreateFollowerDto,
+  ): Promise<Followers | string> {
+    return this.followerService.update(createFollowerDto, 'Declined');
+  }
+
+  @Delete('unfollow')
+  delete(
+    @Body() createFollowerDto: CreateFollowerDto,
+  ): Promise<Followers | string> {
+    return this.followerService.delete(createFollowerDto);
   }
 }

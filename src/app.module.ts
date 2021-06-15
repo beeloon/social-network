53b4f7911ccd37d114e64cf4fda667b3b7ae8394
Yegor Tsukanov <69nonesons@gmail.com>
2021-06-15 12:34:51 +0300

    Refactor app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,19 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PostModule } from './modules/post/post.module';
 import { FollowerModule } from './modules/follower/follower.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './database/entities/user';
-import { Followers } from './database/entities/followers';
+
+import { DatabaseModule } from './database/database.module';
+
+import configuration from './config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/social-network', {
-      useCreateIndex: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => ({
-        type: 'mysql',
-        host: process.env.SQL_DB_HOST,
-        port: +process.env.SQL_DB_PORT,
-        username: process.env.SQL_DB_USERNAME,
-        password: process.env.SQL_DB_PASSWORD,
-        database: process.env.SQL_DB_DATABASE,
-        entities: [User, Followers],
-        synchronize: true,
-        logging: ['query', 'error'],
-        logger: 'file',
-      }),
-    }),
+    ConfigModule.forRoot({ load: [configuration] }),
+    DatabaseModule,
     UserModule,
     PostModule,
     AuthModule,

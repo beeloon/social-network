b73aadd47e53204eac75981b4c480e7920cd6f0b
Ilya <sheverdinkin@gmail.com>
2021-06-16 16:38:46 +0300

    merge commit

--- src/database/entities/user-entity.ts
+++ src/database/entities/user-entity.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import {
   Entity,
   Column,
@@ -7,15 +8,14 @@ import {
   Unique,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import * as bcrypt from 'bcrypt';
-import { CreateUserDto } from 'src/modules/user/dto/create-user-dto';
+import { CreateUserDto } from '../../modules/user/dto/create-user-dto';
 
 @Unique(['id'])
 @Unique(['email'])
-@Entity({ name: 'users' }) // Name of your table in database
+@Entity({ name: 'users' })
 export class User {
-  constructor(createDto: CreateUserDto){
-    Object.assign(this, createDto);
+  constructor(userDto: CreateUserDto) {
+    Object.assign(this, userDto);
   }
   @PrimaryGeneratedColumn('uuid')
   id: string;
@@ -29,15 +29,12 @@ export class User {
   @Column()
   password: string;
 
-<<<<<<< HEAD
-   @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', update: false })
-   updatedAt: Date;
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp', update: false })
+  updatedAt: Date;
 
-   @CreateDateColumn({ name: 'created_at', type: 'timestamp', update: false })
-   createdAt: Date;
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp', update: false })
+  createdAt: Date;
 
-=======
->>>>>>> c1f32e4a08bb3c574cfa8696fbca6aaf5dc9e3cf
   @BeforeInsert()
   async beforeInsertActions() {
     this.password = await bcrypt.hash(this.password, 10);
